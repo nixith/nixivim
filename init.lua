a29c81a4589d06c07f7b9da1e1741b0dc4143338
nix <rwstauffer72@gmail.com>
2024-12-13 12:43:55 -0500

    fix lzn auto load

--- init.lua
+++ init.lua
@@ -126,6 +126,6 @@ require("plugins.init.text")
 require("plugins.init.ui")
 require("plugins.init.which_key")
 
-require("lzn-auto-require.loader").register_loader()
+require("lzn-auto-require.loader").enable()
 -- set colorscheme on open
 vim.cmd.colorscheme("catppuccin-mocha")
