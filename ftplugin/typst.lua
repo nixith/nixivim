a8906ac1aba32b753de4bb805c89e356afe4df88
nix <rwstauffer72@gmail.com>
2025-01-01 20:37:01 -0500

    fix plugin name

--- ftplugin/typst.lua
+++ ftplugin/typst.lua
@@ -1,4 +1,4 @@
-require("typst-preview.nvim").setup({
+require("typst-preview").setup({
 	dependencies_bin = {
 		["tinymist"] = vim.env.TINYMIST_PATH, --NOTE: Relies on env var, want to move to category definition at some point if I can figure out how to mix explicit and implicit category stuffs.
 		["websocat"] = vim.env.WEBSOCAT_PATH,
