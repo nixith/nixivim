c83e43a1a03ff0c6685482944a2041d179ad6e88
nixith <rwstauffer72@gmail.com>
2026-04-03 11:31:25 -0400

    add some shells

---
+++ release-shell.nix
@@ -0,0 +1,14 @@
+{ }:
+let
+  pins = import ./npins;
+  pkgs = import pins.nixpkgs { };
+  nixivim = import ./default.nix;
+
+in
+pkgs.mkShellNoCC {
+
+  packages = [
+    nixivim.packages.${pkgs.stdenv.hostPlatform.system}.nvim
+  ];
+
+}
