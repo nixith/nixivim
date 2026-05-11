a0601518914eb508b0c31316af0aa10bf596746a
nixith <rwstauffer72@gmail.com>
2026-05-11 11:37:32 -0400

    enable ui2

--- init.lua
+++ init.lua
@@ -88,6 +88,54 @@ if vim.fn.exists("&pumborder") == 1 then
 	vim.o.pumborder = "rounded"
 end
 
+vim.o.cmdheight = 0
+require("vim._core.ui2").enable({
+	enable = true,
+	msg = {
+		targets = {
+			[""] = "msg",
+			empty = "cmd",
+			bufwrite = "msg",
+			confirm = "cmd",
+			emsg = "pager",
+			echo = "msg",
+			echomsg = "msg",
+			echoerr = "pager",
+			completion = "cmd",
+			list_cmd = "pager",
+			lua_error = "pager",
+			lua_print = "msg",
+			progress = "pager",
+			rpc_error = "pager",
+			quickfix = "msg",
+			search_cmd = "cmd",
+			search_count = "cmd",
+			shell_cmd = "pager",
+			shell_err = "pager",
+			shell_out = "pager",
+			shell_ret = "msg",
+			undo = "msg",
+			verbose = "pager",
+			wildlist = "cmd",
+			wmsg = "msg",
+			typed_cmd = "cmd",
+		},
+		cmd = {
+			height = 0.5,
+		},
+		dialog = {
+			height = 0.5,
+		},
+		msg = {
+			height = 0.3,
+			timeout = 5000,
+		},
+		pager = {
+			height = 0.5,
+		},
+	},
+})
+
 --TODO: figure out a better trigger system for TS supported buffers
 -- treesitter
 vim.api.nvim_create_autocmd("BufNew", {
